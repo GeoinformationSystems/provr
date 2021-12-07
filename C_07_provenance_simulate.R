9f32fa84f168330d95e20b78432d322a08e3f573
rue_a <arne.ruemmler@gmail.com>
2021-12-07 13:57:38 +0100

    initial commit

---
+++ C_07_provenance_simulate.R
@@ -0,0 +1,199 @@
+##############################################################
+##############################################################
+##############################################################
+##############################################################
+####################  R script C_07 ##########################
+##############################################################
+##############################################################
+##############################################################
+
+## SUMMARY: the script is about finding and storing the 5 x 5 km least cloudy Landsat image per year (based on GLASS_GLC grids)
+## where the validation data are available.
+
+## load libraries
+# library(sf)
+# library(raster)
+# library(sp)
+# library(rgee)
+# library(tidyverse)
+
+library(devtools)
+load_all()
+init_provenance_graph(namespace = "https://www.idiv.de/de/luckinet.html/geokur/C07#")
+
+
+## loading the pre-selected GLASS-GLC grids to download Landsat images
+# dir <- "//idiv.de/public/homes/cb58hypi/GeoKur/2021-2022/04. Information/1.Chapter/GeoKur_case_study/28102021/provenance_CB/data"
+# files <- list.files(dir, ".shp", full.names = TRUE)
+
+## creating the full file
+# file <- NULL
+# for (j in files) {
+#   year <- sub(".*grid_Landsat_", "", j)
+#   year <- as.numeric(substr(year, 1, 4))
+#   x <- st_read(j)
+#   comb <- cbind(x, year)
+#   file <- rbind(file, comb)
+# }
+
+
+## clean duplicates
+# file <- file %>% distinct(cell_ID, year, .keep_all = TRUE)
+
+pre_sel_grids_entity <- Entity(id = "pre_selected_grids", label = "pre selected grids")
+remove_duplicates_activity <- Activity(id = "remove_duplicated", label = "remove duplicates")
+remove_duplicates_activity$used(pre_sel_grids_entity)
+
+cleaned_grids_entity <- Entity("cleaned_grids", "landsat image collection", description = "yearly landsat image collection for each grid cell")
+cleaned_grids_entity$wasGeneratedBy(remove_duplicates_activity)
+
+
+## Connect with Google Earth Engine
+# ee_Initialize(email = "caterina.barrasso@gmail.com")
+
+## Functions to filter out the clouds later
+
+# maskL875sr <- function(image) {
+#   cloudShadowBitMask <- 3
+#   cloudsBitMask <- 5
+
+#   ## Select the QA band.
+#   qa <- image$
+#     select("pixel_qa")
+
+#   ## Get the internal_cloud_algorithm_flag bit.
+#   mask <- qa$bitwiseAnd(cloudShadowBitMask)$
+#     eq(0)$And(qa$bitwiseAnd(cloudsBitMask)$
+#     eq(0))
+
+#   ## Return an image masking out cloudy areas
+#   image$
+#     updateMask(mask$Not())$
+#     select("B.*")
+# }
+
+# setwd("//idiv.de/public/homes/cb58hypi/GeoKur/2021-2022/04. Information/1.Chapter/GeoKur_case_study/28102021/provenance_CB/data/Landsat")
+
+# ## Dividing the file in chuncks to download the images
+# max <- 50
+# x <- nrow(file)
+# rows <- ceiling(x / max)
+# list <- split(file, (as.numeric(rownames(file)) - 1) %/% rows)
+
+# ## Starting the iteration
+# i <- as.integer(Sys.getenv("SLURM_ARRAY_TASK_ID"))
+
+# subset <- list[[i]]
+# print("subset created")
+# rm(list = (ls()[ls() != "subset"]))
+
+# download Landsat
+# for (j in 1:nrow(subset)) {
+for (j in 1:10) {
+  # cell <- subset[j, ]
+
+  # ## check if the image was already downloaded
+  # setwd(paste0("//idiv.de/public/homes/cb58hypi/GeoKur/2021-2022/04. Information/1.Chapter/GeoKur_case_study/28102021/provenance_CB/data/Landsat/", cell$year))
+  # files <- list.files(".", ".tif")
+  # tile <- sub(".*cell_ID_", "", files)
+  # tile <- as.numeric(str_remove(tile, ".tif"))
+
+  # if (cell$cell_ID %in% tile) {
+  #   print("already downloaded")
+  # }
+  if (0) {
+  } else {
+
+    #   ## otherwise download it and decide with Landsat collection to use depending on the year
+    #   if (cell$year == 2012) {
+    #     dataset <- ee$ImageCollection("LANDSAT/LE07/C01/T1_SR")
+    #   } else if (cell$year > 2012) {
+    #     dataset <- ee$ImageCollection("LANDSAT/LC08/C01/T1_SR")
+    #   } else {
+    #     dataset <- ee$ImageCollection("LANDSAT/LT05/C01/T1_SR")
+    #   }
+
+    #   start <- paste0(cell$year, "-01-01")
+    #   end <- paste0(cell$year, "-12-31")
+
+    #   Landsat <- dataset$
+    #     filterDate(start, end)
+
+    #   object <- sf_as_ee(cell)
+
+    #   geom_params <- list(
+    #     scale = 30,
+    #     crs = "EPSG:4326",
+    #     region = object$geometry()
+    #   )
+
+
+    #   Landsat_cropped <- Landsat$filterBounds(object)$sort("CLOUDY_PIXEL_PERCENTAGE")$mosaic()
+
+
+
+    #   no_clouds <- maskL875sr(Landsat_cropped)
+
+    # build new process node in each loop, but give the same label
+    # semantically that means something like: it is the same process, but different instantiations
+    id <- paste("sel_least_cloudy_img", j, sep = "_")
+    sel_least_cloudy_img_activity <- Activity(id, "select least cloudy image")
+    sel_least_cloudy_img_activity$used(cleaned_grids_entity)
+
+    id <- paste("least_cloudy_img", j, sep = "_")
+    label <- paste("least cloudy image of the year", j, sep = " ")
+    least_cloudy_img_entity <- Entity(id, label)
+    least_cloudy_img_entity$wasGeneratedBy(sel_least_cloudy_img_activity)
+    # Download the images and prepare the stack
+
+    try(
+      {
+        # path <- no_clouds$getDownloadUrl(geom_params)
+
+        # dir.create(paste0("//idiv.de/public/homes/cb58hypi/GeoKur/2021-2022/04. Information/1.Chapter/GeoKur_case_study/28102021/provenance_CB/data/Landsat/", cell$year))
+
+        # download.file(path, paste0("//idiv.de/public/homes/cb58hypi/GeoKur/2021-2022/04. Information/1.Chapter/GeoKur_case_study/28102021/provenance_CB/data/Landsat/", cell$year, "/GLASS-GLC_cell_ID_", cell$cell_ID, ".zip"), mode = "wb")
+
+        # dir.create(paste0("//idiv.de/public/homes/cb58hypi/GeoKur/2021-2022/04. Information/1.Chapter/GeoKur_case_study/28102021/provenance_CB/data/Landsat/", cell$year, "/GLASS-GLC_cell_ID_", cell$cell_ID))
+
+        # unzip(paste0("//idiv.de/public/homes/cb58hypi/GeoKur/2021-2022/04. Information/1.Chapter/GeoKur_case_study/28102021/provenance_CB/data/Landsat/", cell$year, "/GLASS-GLC_cell_ID_", cell$cell_ID, ".zip"), exdir = paste0("//idiv.de/public/homes/cb58hypi/GeoKur/2021-2022/04. Information/1.Chapter/GeoKur_case_study/28102021/provenance_CB/data/Landsat/", cell$year, "/GLASS-GLC_cell_ID_", cell$cell_ID))
+
+        # setwd(paste0("//idiv.de/public/homes/cb58hypi/GeoKur/2021-2022/04. Information/1.Chapter/GeoKur_case_study/28102021/provenance_CB/data/Landsat/", cell$year))
+
+        # unlink(paste0("GLASS-GLC_cell_ID_", cell$cell_ID, ".zip"))
+
+        # setwd(paste0("//idiv.de/public/homes/cb58hypi/GeoKur/2021-2022/04. Information/1.Chapter/GeoKur_case_study/28102021/provenance_CB/data/Landsat/", cell$year, "/GLASS-GLC_cell_ID_", cell$cell_ID))
+
+        # rasterFiles <- list.files(pattern = "tif")
+
+        # stack <- stack(rasterFiles)
+
+        # setwd(paste0("//idiv.de/public/homes/cb58hypi/GeoKur/2021-2022/04. Information/1.Chapter/GeoKur_case_study/28102021/provenance_CB/data/Landsat/", cell$year))
+
+        # writeRaster(stack, paste0("GLASS-GLC_cell_ID_", cell$cell_ID, ".tiff"))
+
+        # dir_to_clean <- paste0("//idiv.de/public/homes/cb58hypi/GeoKur/2021-2022/04. Information/1.Chapter/GeoKur_case_study/28102021/provenance_CB/data/Landsat/", cell$year, "/GLASS-GLC_cell_ID_", cell$cell_ID)
+
+        # file.remove(dir(dir_to_clean, pattern = "download", full.names = TRUE))
+
+        # unlink(paste0("GLASS-GLC_cell_ID_", cell$cell_ID), recursive = TRUE)
+
+
+        id <- paste("mask_out_clouds_and_shadows", j, sep = "_")
+        mask_out_clouds_activity <- Activity(id, "mask out clouds and shadows")
+        mask_out_clouds_activity$used(least_cloudy_img_entity)
+
+        id <- paste("processed_landsat_img", j, sep = "_")
+        label <- paste("processed landsat image", j, sep = " ")
+
+        processed_landsat_img_entity <- Entity(id, label)
+        processed_landsat_img_entity$wasGeneratedBy(mask_out_clouds_activity)
+      },
+      silent = F
+    )
+  }
+
+  # print(j)
+}
+
+serialize_provenance_graph(name = "catarina.ttl")
---
+++ notes/notes.md
@@ -0,0 +1,5 @@
+steffen fine grained: ca. 200 lines
+steffen coarse grained: ca. 70 lines
+
+steffen scripts 0-6: ca. 1000 lines
+
